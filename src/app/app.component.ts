5b061b1433e132c122e0fb29a81a4b90634a944f
Luendel <luendel3@hotmail.com>
2024-11-08 18:26:10 -0300

    modified:   src/app/app.component.html modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarModule } from '../components/navbar/navbar.component.module';
 import { HeroSectionModule } from '../components/hero-section/hero-section.component.module';
 import { NewSectionModule } from '../components/new-section/section.component.module';
+import { PopularModule } from '../components/popular/popular.component.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarModule, HeroSectionModule, NewSectionModule],
+  imports: [RouterOutlet, NavbarModule, HeroSectionModule, NewSectionModule, PopularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
