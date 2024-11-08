434188ada1f20d6092041be707401aad2b3a5058
Luendel <luendel3@hotmail.com>
2024-11-08 17:01:09 -0300

    modified:   src/app/app.component.html modified:   src/app/app.component.ts new file:   src/components/hero-section/hero-section.component.css new file:   src/components/hero-section/hero-section.component.html new file:   src/components/hero-section/hero-section.component.module.ts new file:   src/components/hero-section/hero-section.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarModule } from '../components/navbar/navbar.component.module';
+import { HeroSectionModule } from '../components/hero-section/hero-section.component.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarModule],
+  imports: [RouterOutlet, NavbarModule, HeroSectionModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/components/hero-section/hero-section.component.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core"
+import { HeroSection } from "./hero-section.component";
+
+@NgModule({
+    declarations:[HeroSection],
+    exports:[HeroSection]
+})
+export class HeroSectionModule {
+
+}
