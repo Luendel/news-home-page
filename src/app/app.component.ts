7322132d4bc451dadec7413a7674135a65a43232
Luendel <luendel3@hotmail.com>
2024-11-11 22:50:38 -0300

    modified:   src/app/app.component.html modified:   src/app/app.component.ts new file:   src/components/aside-nav/aside-nav.component.css new file:   src/components/aside-nav/aside-nav.component.html new file:   src/components/aside-nav/aside-nav.component.module.ts new file:   src/components/aside-nav/aside-nav.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarModule } from '../components/navbar/navbar.component.module';
 import { HeroSectionModule } from '../components/hero-section/hero-section.component.module';
 import { NewSectionModule } from '../components/new-section/section.component.module';
 import { PopularModule } from '../components/popular/popular.component.module';
+import { AsideNavModule } from '../components/aside-nav/aside-nav.component.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarModule, HeroSectionModule, NewSectionModule, PopularModule],
+  imports: [NavbarModule, HeroSectionModule, NewSectionModule, PopularModule,AsideNavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/components/aside-nav/aside-nav.component.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core"
+import { AsideNav } from "./aside-nav.component";
+
+
+@NgModule({
+    declarations:[AsideNav],
+    exports:[AsideNav]
+})
+export class AsideNavModule {
+    
+}
