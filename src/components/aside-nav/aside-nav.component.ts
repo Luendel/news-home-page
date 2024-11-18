8d574b8b47d94cc143f2e402e32872140945fd40
Luendel <luendel3@hotmail.com>
2024-11-18 04:06:46 -0300

    modified:   src/app/app.component.html modified:   src/components/aside-nav/aside-nav.component.html modified:   src/components/aside-nav/aside-nav.component.ts

--- src/components/aside-nav/aside-nav.component.ts
+++ src/components/aside-nav/aside-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector:"app-aside-nav",
@@ -7,4 +7,9 @@ import { Component, Input } from "@angular/core";
 })
 export class AsideNav {
     @Input() display: any;
+    @Output() toggle = new EventEmitter<string>()
+
+    closeAsideNav(){
+        this.toggle.emit("toggleAsideNav")
+    }
 }
